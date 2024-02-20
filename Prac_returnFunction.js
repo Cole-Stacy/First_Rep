b51aaaaee417e3d153d401c40408136a56076dfd
Cole-Stacy <62519404+Cole-Stacy@users.noreply.github.com>
2024-02-20 12:52:26 -0600

    Update Prac_returnFunction.js
    Improved Code

--- Prac_returnFunction.js
+++ Prac_returnFunction.js
@@ -1,8 +1,5 @@
 function getMilk(starting_money,cost_per_bottle){
-    amountBuy=Math.floor(starting_money/cost_per_bottle)
-    totalCost = amountBuy*cost_per_bottle
-    remaining = starting_money - totalCost
-    return remaining
+    return starting_money % cost_per_bottle
 }
 
 change = getMilk(1000, 3.68)

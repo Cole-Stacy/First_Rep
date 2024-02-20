f725bcf4cadce7e5945210fac9c75674b3f1af34
Cole-Stacy <62519404+Cole-Stacy@users.noreply.github.com>
2024-02-20 13:36:16 -0600

    Update randomNumber.js
    Added from Project "Love Calculator"

--- randomNumber.js
+++ randomNumber.js
@@ -1,5 +1,16 @@
+function loveCalculator(){
+    person1 = prompt("what is the name of the first person?");
+    person2 = prompt("Name of Second Person");
+
+    lovecal=RandomNumber(100);
+    alert("You have a " + lovecal +"% chance of Falling in Love");
+    
+}
+
 function RandomNumber (number){
     multiplier = Math.random();
-    randomN = Math.floor(number*multiplier);
-    return randomN;
+    adjustment = Math.floor(number*multiplier)+1;
+    return adjustment;
 }
+
+//loveCalculator()

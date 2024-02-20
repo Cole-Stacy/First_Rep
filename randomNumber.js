81e523dab91de8660f7b444aa482dbf96539a450
Cole-Stacy <62519404+Cole-Stacy@users.noreply.github.com>
2024-02-20 13:52:00 -0600

    Update randomNumber.js
    added Conditionals to outcomes

--- randomNumber.js
+++ randomNumber.js
@@ -3,7 +3,19 @@ function loveCalculator(){
     person2 = prompt("Name of Second Person");
 
     lovecal=RandomNumber(100);
-    alert("You have a " + lovecal +"% chance of Falling in Love");
+    if (lovecal === 100){
+        alert("You have a " + lovecal +"% chance of Falling in Love, You both are going to make it");
+    } 
+    if (lovecal>=75 && lovecal < 100){
+        alert("You have a " + lovecal +"% chance of Falling in Love...you might make it.");
+    }
+    if (lovecal>=50 && lovecal < 75){
+        alert("You have a " + lovecal +"% chance of Falling in Love....a probably not going to make it");
+    } else{
+        alert("You have a " + lovecal +"% chance of Falling in Love...not going to make it.");
+    }
+    
+    
     
 }
 
